933d796fd281c42fa43c5b1b9d7f5a6052884411
Phu Nguyen Minh <57850282+wwphuztr@users.noreply.github.com>
2023-05-28 16:24:51 +0700

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,30 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  try {
+    await page.goto('https://playwright.dev/');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  } catch (error) {
+    console.error('Test failed:', error); // Logging the error
+    throw error; // Rethrow the error to mark the test as failed
+  }
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  
+  try {
+    await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.*intro/);
+  } catch (error) {
+    console.error('Test failed:', error); // Logging the error
+    throw error; // Rethrow the error to mark the test as failed
+  }
 });
